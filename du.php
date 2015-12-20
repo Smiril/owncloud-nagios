e5c992933b021e1c019951dcac2c039282b330ca
Smiril <Smiril@users.noreply.github.com>
2015-12-20 17:03:56 +0100

    Update du.php

--- du.php
+++ du.php
@@ -5,8 +5,26 @@ $password = "pwd";
 $hostname = "localhost"; 
 $database = "owncloud";
 $location = "/srv/www/owncloud/data/"; //Change this to the location where userdirs are located on your owncloud linux server
+function formatBytes($size1, $decimals = 0){
+    $unit = array(
+        '0' => 'Byte',
+        '1' => 'KB',
+        '2' => 'MB',
+        '3' => 'G',
+        '4' => 'TB',
+        '5' => 'PB',
+        '6' => 'EB',
+        '7' => 'ZB',
+        '8' => 'YB'
+    );
 
-//connection to the database
+    for($i = 0; $size1 >= 1024 && $i <= count($unit); $i++){
+        $size1 = $size1/1024;
+    }
+
+    return round($size1, $decimals).'';
+}
+
 $dbhandle = mysql_connect($hostname, $username, $password)
  or die("Unable to connect to MySQL");
 //select a database to work with
@@ -18,7 +36,7 @@ while ($row = mysql_fetch_array($allusers)) {
         $allus = $row{'userid'};
 //check on disk to see directory size
     $dirname = $location.$allus;
-    $io = popen ( '/usr/bin/du -sh ' . $dirname, 'r' );
+    $io = popen ( '/usr/bin/du -sb ' . $dirname, 'r' );
     $size = fgets ( $io , 4096);
     $sizecurb = substr ( $size, 0, strpos ( $size, "\t" ) );
     pclose ( $io );
@@ -28,14 +46,14 @@ $actquot = mysql_fetch_array($quota);
 //quota prolly in GB values, for nagios i want bytes
 if (strpos($actquot{'configvalue'},'GB') !== false) {
     list($sizecur, $unit) = explode(" ", $actquot{'configvalue'});
-        $maxquot = $sizecur;
+        $maxquot = $sizecur * 1024 * 1024 * 1024;
 }
 //user has no custom quota, use the system default of 10 GB
 else {
         $maxquot = $sizeuser ; 
 }
 //print username, current usage in bytes, and the max available space in bytes
-echo $allus.",".$sizecurb.",".$maxquot."G";
+echo $allus."-".formatBytes($sizecurb, 3)."-".$maxquot."";
 }
 //close the connection
 mysql_close($dbhandle);
