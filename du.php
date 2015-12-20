546814a137ddae5347906bbb09957910d860d11a
Smiril <Smiril@users.noreply.github.com>
2015-12-20 14:25:38 +0100

    Update du.php
    make it human readable
    
    like: Mb used 13G allowed: 1024G

--- du.php
+++ du.php
@@ -1,9 +1,10 @@
 <?php
-$username = "mysql";
-$password = "passwd";
+$sizeuser = "1024"; // in Gigabyte storage size
+$username = "user";
+$password = "pwd";
 $hostname = "localhost"; 
 $database = "owncloud";
-$location = "/mnt/owncloud/owncloud/"; //Change this to the location where userdirs are located on your owncloud linux server
+$location = "/srv/www/owncloud/data/"; //Change this to the location where userdirs are located on your owncloud linux server
 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password)
@@ -12,41 +13,30 @@ $dbhandle = mysql_connect($hostname, $username, $password)
 $selected = mysql_select_db($database, $dbhandle)
   or die("Could not select owncloud");
 //execute the SQL query and return records
-$allusers = mysql_query("select distinct userid from oc_preferences where configkey = 'firstLoginAccomplished' ;");
+$allusers = mysql_query("select distinct userid from oc_preferences where configkey = 'lastLogin' ;");
 while ($row = mysql_fetch_array($allusers)) {
-	$allus = $row{'userid'};
-
+        $allus = $row{'userid'};
 //check on disk to see directory size
     $dirname = $location.$allus;
-    $io = popen ( '/usr/bin/du -sb ' . $dirname, 'r' );
-    $size = fgets ( $io, 4096);
+    $io = popen ( '/usr/bin/du -sh ' . $dirname, 'r' );
+    $size = fgets ( $io , 4096);
     $sizecurb = substr ( $size, 0, strpos ( $size, "\t" ) );
     pclose ( $io );
-
 // check if user has higher then default quota
 $quota = mysql_query("select configvalue from oc_preferences where configkey = 'quota' and userid = '$allus' ;");
 $actquot = mysql_fetch_array($quota);
 //quota prolly in GB values, for nagios i want bytes
 if (strpos($actquot{'configvalue'},'GB') !== false) {
     list($sizecur, $unit) = explode(" ", $actquot{'configvalue'});
-	$maxquot = $sizecur * 1024 * 1024 * 1024;
+        $maxquot = $sizecur;
 }
 //user has no custom quota, use the system default of 10 GB
 else {
-	$maxquot = 10 * 1024 * 1024 * 1024;
+        $maxquot = $sizeuser ; 
 }
-
 //print username, current usage in bytes, and the max available space in bytes
-echo $allus.",".$sizecurb.",".$maxquot."
-";
-
+echo $allus.",".$sizecurb.",".$maxquot."G";
 }
-
 //close the connection
 mysql_close($dbhandle);
-
-
-
-
-
 ?>
